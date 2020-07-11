52444ae33af8e2e32d9b98beb5cc3457a9c44ddc
shahidsiddiqui786 <56132385+shahidsiddiqui786@users.noreply.github.com>
2020-07-11 10:43:07 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+ $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+$(document).ready(function(){
+  $("#loginButton").click(function(){
+    $("#loginModal").modal();
+  });
+});
+$(document).ready(function(){
+  $("#reserveTableButton").click(function(){
+    $("#reserveTable").modal();
+  });
+});
